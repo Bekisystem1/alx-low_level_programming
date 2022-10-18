5d677db335244dd9a4ac1d64f06cbc33591ee1bf
Bekisystem1 <bereket.oldemaf@gmail.com>
2022-10-18 12:02:18 +0000

    0-putchar.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int n;
+int i;
+for (n = 0; n < 10; n++)
+{
+	for (i = 0; i < 10; i++)
+	{
+
+	putchar((n % 10) + '0');
+	putchar((i % 10) + '0');
+	putchar(',');
+	putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
