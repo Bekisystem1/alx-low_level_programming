5098ceb49ff59f09a617b7c43268d83b72aba2c9
Bekisystem1 <bereket.oldemaf@gmail.com>
2022-10-19 21:48:04 +0000

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,11 @@
-#include <stdio.h>
-/**
-   * main - Entry point
-    *
-     * Return: Always 0 (success)
-      */
 #include "main.h"
-void print_alphabet(void);
-int main(void)
-{
-	print_alphabet();
-	putchar('\n');
-	return (0);
-}
+
 /**
-   * print_alphabet - Print lowercase letters
+   * print_alphabet - Prints lowwercase letters
     *
-     */
+     *
+      */
+
 void print_alphabet(void)
 {
 	char c;
@@ -24,4 +14,5 @@ void print_alphabet(void)
 	{
 		putchar(c);
 	}
+	putchar('\n');
 }
