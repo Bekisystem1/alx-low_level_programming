8019794703458903e4272d3c08b7126188346e74
Bekisystem1 <bereket.oldemaf@gmail.com>
2022-10-20 09:24:06 +0000

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+   * _isdigit - Return 1 if c is digit (0-9)
+    * @c: Digit to be checked
+     *
+      *Return: 1 if c is digit. otherwise 0
+       */
+int _isdigit(int c)
+{
+	if (c < 58 && c > 47)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
+
