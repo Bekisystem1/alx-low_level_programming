b1adf36c1e03325205bd3916a98e5d6f323d58c2
Bekisystem1 <bereket.oldemaf@gmail.com>
2022-10-20 21:34:50 +0000

    6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+   * print_line - Print straight line
+    *@n: Number of times the character is printed
+     *
+      */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n != 0 || n > 0)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
+
