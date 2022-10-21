a64a9860e8aad34e3fb920062b572eeb1038b29c
Bekisystem1 <bereket.oldemaf@gmail.com>
2022-10-21 15:04:38 +0000

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+/**
+   * main - check the code
+    *
+     * Return: Always 0.
+      */
+void print_natural_numbers(void);
+int main(void)
+{
+	print_natural_numbers();
+	return (0);
+}
+/**
+   * print_natural_numbers - Print numbers from 1 to 100
+    *
+     */
+void print_natural_numbers(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		printf(" ");
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if
+		(i % 3 == 0)
+		{
+			printf("fizz");
+		}
+		else if
+		(i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+}
+
+
+
