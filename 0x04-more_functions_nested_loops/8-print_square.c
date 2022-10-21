e96d06ec06d42be978e294b943abcee7ea92eecb
Bekisystem1 <bereket.oldemaf@gmail.com>
2022-10-21 12:32:24 +0000

    8-square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+   * print_square - print slash
+    * @size: Number of times square is printed
+     *
+      */
+void print_square(int size)
+{
+int i, j;
+if (size <= 0)
+	_putchar('\n');
+for (i = 0; i < size; i++)
+{
+	for (j = 0; j < size; j++)
+		_putchar('#');
+	_putchar('\n');
+}
+}
