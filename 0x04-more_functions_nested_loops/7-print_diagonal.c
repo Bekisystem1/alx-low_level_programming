46ad45657e83d5e423211bf6095fd57671782085
Bekisystem1 <bereket.oldemaf@gmail.com>
2022-10-21 11:41:55 +0000

    7-diagonals.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,29 +1,24 @@
 #include "main.h"
 /**
-   *print_line - print slash
-    *@n: Number of times slash is printed
+   * print_diagonal - print slash
+    * @n: Number of times slash is printed
      *
       */
-void print_line(int n)
+void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 1; i <= n; i++)
+	if (n <= 0)
+
+		_putchar('\n');
+	for (i = 0; i < n; i++)
 	{
-		if (n != 0 || n > 0)
-		{
-			_putchar('\\');
-			_putchar('\n');
-			for (j = 0; j < i; j++)
-			{
-				_putchar(' ');
-			}
-		}
-		else
-		{
-			_putchar('\n');
-		}
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+
+		_putchar('\\');
+		_putchar('\n');
+
 	}
-	_putchar('\n');
 }
 
